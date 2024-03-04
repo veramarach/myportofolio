3042f78d83dd097fc2375af3fffdcebb38a614f1
veramarach <edemahd@gmail.com>
2024-03-04 13:19:52 -0800

    project component added

--- src/Pages/LandingPage.tsx
+++ src/Pages/LandingPage.tsx
@@ -3,12 +3,14 @@ import About from '../Components/About'
 import Hero from '../Components/Hero'
 
 import Contact from '../Components/Contact'
+import Projects from '../Components/Projects'
 
 const LandingPage :React.FC= () => {
   return (
     <div>
         <Hero/>
         <About/>
+        <Projects/>
         <Contact/>
         
 
