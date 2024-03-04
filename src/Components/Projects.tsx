67d74f0cb62b186a461e3d2ce08108a47b0e5b3d
veramarach <edemahd@gmail.com>
2024-03-04 10:50:31 -0800

    my portofolio components

---
+++ src/Components/Projects.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Projects:React.FC= () => {
+  return (
+    <div id='project' className="bg-white w-full h-[100vh] flex justify-center py-8">
+        <div className='flex justify-center'>
+       <h2 className='text-white font-bold text-5xl absolute mt-2 '>Projects</h2>
+        <div className='w-[150px] h-[25px] bg-[#D63484]  mt-8 ml-20'></div>
+       </div>
+
+    </div>
+  )
+}
+
+export default Projects
---
+++ src/Pages/LandingPage.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import About from '../Components/About'
+import Hero from '../Components/Hero'
+
+import Contact from '../Components/Contact'
+
+const LandingPage :React.FC= () => {
+  return (
+    <div>
+        <Hero/>
+        <About/>
+        <Contact/>
+        
+
+    </div>
+  )
+}
+
+export default LandingPage
